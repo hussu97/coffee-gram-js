17e41617f2a9459b0edf441267a90eac180678e3
hussu97 <h_abbasi97@hotmail.com>
2018-01-27 12:43:43 +0400

    Added basic layout, and functionality to let user add new campsite

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 const port = 3000;
+
+var campgrounds = [
+  {name: "New Wark", image:"https://source.unsplash.com/5dehYy5BkRw"},
+  {name: "Holy Cow", image:"https://source.unsplash.com/26f8ZvTWV4E"},
+  {name: "WOWOWO", image:"https://source.unsplash.com/TwIXMn7iNnw"}
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -7,7 +17,21 @@ app.get('/', (req, res) => {
   res.render("landing");
 });
 app.get('/campgrounds', (req, res) => {
+  res.render("campgrounds",{campgrounds:campgrounds});
+});
+app.get('/campgrounds/new', (req, res) => {
+  //Showing the form that the user can fill out
+  res.render("new");
+});
 
+app.post('/campgrounds', (req, res) => {
+  //A form is created to allow user to add a new campground
+  var name = req.body.name;
+  var url = req.body.image;
+  var newCampground = {name:name,image:url};
+  campgrounds.push(newCampground)
+  //redirect back to campgrounds page
+  res.redirect('/campgrounds');
 });
 
 app.listen(port, () => {
