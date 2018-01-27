487004f1cabfc25ea8be87883da1650881aeaee6
hussu97 <h_abbasi97@hotmail.com>
2018-01-27 21:41:30 +0400

    Added brand logo, updated css for new campground form using bootstrap

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = 8080;
+const favicon = require('serve-favicon');
+const path = require('path');
 
 var campgrounds = [
+  {name: "New Wark", image:"https://source.unsplash.com/5dehYy5BkRw"},
+  {name: "Holy Cow", image:"https://source.unsplash.com/26f8ZvTWV4E"},
+  {name: "WOWOWO", image:"https://source.unsplash.com/oT4hTqWoZ6M"},
+  {name: "New Wark", image:"https://source.unsplash.com/5dehYy5BkRw"},
+  {name: "Holy Cow", image:"https://source.unsplash.com/26f8ZvTWV4E"},
+  {name: "WOWOWO", image:"https://source.unsplash.com/TwIXMn7iNnw"},
   {name: "New Wark", image:"https://source.unsplash.com/5dehYy5BkRw"},
   {name: "Holy Cow", image:"https://source.unsplash.com/26f8ZvTWV4E"},
   {name: "WOWOWO", image:"https://source.unsplash.com/TwIXMn7iNnw"}
 ];
 
+app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(favicon(path.join(__dirname, 'public','images', 'favicon.ico')));
 
 app.set('view engine', 'ejs');
 
