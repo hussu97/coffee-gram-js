20151e5c0f11e61f87203d862c3726cda41e0b43
hussu97 <h_abbasi97@hotmail.com>
2018-02-03 17:18:38 +0400

    Added Authentication
    Created User database and added local user authentication using passport

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express    = require('express'),
       port       = 8080,
       favicon    = require('serve-favicon'),
       path       = require('path'),
+      passport   = require('passport'),
+      LocalStrategy = require('passport-local'),
       Coffee     = require('./models/coffee'),
       Comment    = require('./models/comment'),
+      User       = require('./models/user'),
       seedDB     = require('./seeds'),
       mongoose   = require('mongoose');
 
@@ -13,6 +16,23 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(favicon(path.join(__dirname, 'public','images', 'favicon.ico')));
+
+//PASSPORT CONFIGURATION
+app.use(require('express-session')({
+    secret: 'I like trains',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.set('view engine', 'ejs');
 
@@ -26,7 +46,7 @@ app.get('/coffees', (req, res) => {
     if(err){
       console.log(err);
     } else{
-      res.render("coffee/index",{coffees:allCoffee});
+      res.render("coffee/index",{coffees:allCoffee, currentUser:req.user});
     }
   });
 });
@@ -44,7 +64,7 @@ app.get('/coffees/:id', (req, res) => {
   });
 
 });
-app.get('/coffees/:id/comments/new', (req, res) => {
+app.get('/coffees/:id/comments/new', isLoggedIn, (req, res) => {
   Coffee.findById(req.params.id,(err,foundCoffee) =>{
     if(err){
       console.log(err);
@@ -74,7 +94,7 @@ app.post('/coffees', (req, res) => {
   //redirect back to coffees page
   res.redirect('/coffees');
 });
-app.post('/coffees/:id/comments', (req, res) => {
+app.post('/coffees/:id/comments', isLoggedIn, (req, res) => {
   console.log("entered comment POST");
   Coffee.findById(req.params.id,(err,foundCoffee) =>{
     if(err){
@@ -96,6 +116,42 @@ app.post('/coffees/:id/comments', (req, res) => {
   });
 });
 
+//AUTH ROUTES
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+app.post('/register', (req, res) => {
+  var newUser = new User({username:req.body.username});
+  User.register(newUser,req.body.password,(err,user)=>{
+    if(err){
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate("local")(req,res,()=>{
+      res.redirect('coffees');
+    })
+  })
+});
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+app.post('/login',passport.authenticate("local",
+  {
+    successRedirect: '/coffees',
+    failureRedirect: '/login'
+  }), (req, res) => {
+});
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+}
 
 app.listen(port, () => {
   console.log(`Server Starts on ${port}`);
